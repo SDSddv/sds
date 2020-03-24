51f85b01435bb1db885b27cb0983507e4de4beda
OlivierUrrutiaguer <62282505+OlivierUrrutiaguer@users.noreply.github.com>
2020-03-24 17:53:10 +0100

    Do not reset the scrolling region after updating the tree view content

--- src/app/layouts/navtree/navtree.component.ts
+++ src/app/layouts/navtree/navtree.component.ts
@@ -36,7 +36,21 @@ export class NavtreeComponent implements OnInit {
   /* Refreshes the whole tree view widget. */
   refreshTree() {
     if (this.treeView) {
+      /*
+        Memorize the scroll bar position in order to
+        restore it after the tree view update.
+        This is done because updating the tree view data makes
+        the scroll bar to be reset to the top of its scroll region.
+      */
+      let scrollOffset = null;
+      if (this.scrollView && this.scrollView.instance) {
+        scrollOffset = this.scrollView.instance.scrollOffset();
+      }
       this.treeView.instance.option("dataSource", this.sdsService.getNavTree());
+      // Restore the scroll bar position.
+      if (this.scrollView && this.scrollView.instance) {
+        this.scrollView.instance.scrollTo(scrollOffset);
+      }
     }
   }
 
