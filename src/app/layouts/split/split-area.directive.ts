f1683446e89d36ff0356a8e46a318bbe04ec1f98
OlivierUrrutiaguer <62282505+OlivierUrrutiaguer@users.noreply.github.com>
2020-03-26 18:17:09 +0100

    GUI style refactoring

--- src/app/layouts/split/split-area.directive.ts
+++ src/app/layouts/split/split-area.directive.ts
@@ -5,9 +5,6 @@ import {DefaultFlexDirective} from '@angular/flex-layout';
   // tslint:disable-next-line:directive-selector
   selector: '[ngxSplitArea]',
   // tslint:disable-next-line:no-host-metadata-property
-  host: {
-    style: 'overflow: auto;'
-  }
 })
 export class SplitAreaDirective {
   constructor(@Optional() @Self() public flex: DefaultFlexDirective) {}
